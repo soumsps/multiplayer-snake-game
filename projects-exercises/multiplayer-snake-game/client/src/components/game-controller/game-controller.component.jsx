080ed987fd43120c9336b67bf5487e865ab8fe00
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-06-15 14:24:22 +0530

    change little css style

--- projects-exercises/multiplayer-snake-game/client/src/components/game-controller/game-controller.component.jsx
+++ projects-exercises/multiplayer-snake-game/client/src/components/game-controller/game-controller.component.jsx
@@ -2,58 +2,97 @@ import React, { useEffect, memo } from 'react';
 import CustomButton from '../custom-button/custom-button.component';
 import './game-controller.styles.css';
 
-const GameController = ({ snakeRef }) => {
-  const KeyCodes = { LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40 };
-  console.log('game controller');
+const GameController = ({ snakeRef, gameStatus, setGameStatus }) => {
+  const KeyCodes = {
+    LEFT: 37,
+    A: 65,
+    UP: 38,
+    W: 87,
+    RIGHT: 39,
+    D: 68,
+    DOWN: 40,
+    S: 83,
+    ENTER: 13,
+    SPACE: 32,
+  };
 
   useEffect(() => {
     document.onkeydown = handleKeyDown;
   });
 
-  const onLeftButtonPress = () => {
-    if (snakeRef.current.direction === 'right') {
+  const onLeftButtonPress = (snakeRef, gameStatus) => {
+    if (snakeRef.current.direction === 'right' || gameStatus !== 'playing') {
       return;
     }
     snakeRef.current.direction = 'left';
-    console.log('left');
   };
-  const onUpButtonPress = () => {
-    if (snakeRef.current.direction === 'down') {
+
+  const onUpButtonPress = (snakeRef, gameStatus) => {
+    if (snakeRef.current.direction === 'down' || gameStatus !== 'playing') {
       return;
     }
     snakeRef.current.direction = 'up';
-    console.log('up');
   };
 
-  const onDownButtonPress = () => {
-    if (snakeRef.current.direction === 'up') {
+  const onDownButtonPress = (snakeRef, gameStatus) => {
+    if (snakeRef.current.direction === 'up' || gameStatus !== 'playing') {
       return;
     }
     snakeRef.current.direction = 'down';
-    console.log('down');
   };
 
-  const onRightButtonPress = () => {
-    if (snakeRef.current.direction === 'left') {
+  const onRightButtonPress = (snakeRef, gameStatus) => {
+    if (snakeRef.current.direction === 'left' || gameStatus !== 'playing') {
       return;
     }
     snakeRef.current.direction = 'right';
-    console.log('right');
+  };
+
+  const onSpaceButtonPress = (gameStatus, setGameStatus) => {
+    if (gameStatus === 'playing') {
+      setGameStatus('paused');
+    } else if (gameStatus === 'paused') {
+      setGameStatus('playing');
+    }
+  };
+
+  const onEnterButtonPress = (gameStatus, setGameStatus) => {
+    if (gameStatus !== 'playing') {
+      setGameStatus('playing');
+    }
   };
 
   const handleKeyDown = (event) => {
     switch (event.keyCode) {
       case KeyCodes.LEFT:
-        onLeftButtonPress();
+        onLeftButtonPress(snakeRef, gameStatus);
+        break;
+      case KeyCodes.A:
+        onLeftButtonPress(snakeRef, gameStatus);
         break;
       case KeyCodes.UP:
-        onUpButtonPress();
+        onUpButtonPress(snakeRef, gameStatus);
+        break;
+      case KeyCodes.W:
+        onUpButtonPress(snakeRef, gameStatus);
         break;
       case KeyCodes.RIGHT:
-        onRightButtonPress();
+        onRightButtonPress(snakeRef, gameStatus);
+        break;
+      case KeyCodes.D:
+        onRightButtonPress(snakeRef, gameStatus);
         break;
       case KeyCodes.DOWN:
-        onDownButtonPress();
+        onDownButtonPress(snakeRef, gameStatus);
+        break;
+      case KeyCodes.S:
+        onDownButtonPress(snakeRef, gameStatus);
+        break;
+      case KeyCodes.SPACE:
+        onSpaceButtonPress(gameStatus, setGameStatus);
+        break;
+      case KeyCodes.ENTER:
+        onEnterButtonPress(gameStatus, setGameStatus);
         break;
       default:
     }
